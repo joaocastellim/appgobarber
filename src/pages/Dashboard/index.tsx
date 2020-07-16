98fb5bc606edc133c707bcb189f81572aceab2b4
João Castelli <joaao.castelli@gmail.com>
2020-07-16 19:46:40 -0300

    fixed some problems

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { View, Button } from 'react-native';
 
 import { useAuth } from '../../hooks/auth';
+import { Container, Header, HeaderTitle, UserName } from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut } = useAuth();
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>João Castelli</UserName>
+        </HeaderTitle>
+      </Header>
+    </Container>
   );
 };
 
